a5b0ec6f720a4f71b9863bf55970a08c8748fcf4
Dmitry Hais <hadminsk@icloud.com>
2026-08-10 11:44:04 +0200

    feat: add dummyjsonapi

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,23 @@ import { createRoot } from "react-dom/client";
 
 import "./app/styles/variables.css";
 import App from "./App";
+import { store } from "./app/store/store";
+import { Provider } from "react-redux";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+}
